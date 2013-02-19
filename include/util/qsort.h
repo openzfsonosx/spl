b75174ed5d69eb535598b4d914975f59f1abf1bc
Jorgen Lundman <lundman@lundman.net>
2013-02-19 16:51:44 +0900

    Handle kernel qsort

--- include/util/qsort.h
+++ include/util/qsort.h
@@ -25,8 +25,9 @@
 #ifndef _SPL_QSORT_H
 #define _SPL_QSORT_H
 
-#include <linux/sort.h>
+//#include <linux/sort.h>
+extern void qsort(void *a, size_t n, size_t es, int (*cmp)(const void *, const void *));
 
-#define qsort(base, num, size, cmp)	sort(base, num, size, cmp, NULL)
+//#define qsort(base, num, size, cmp)	sort(base, num, size, cmp, NULL)
 
 #endif /* SPL_QSORT_H */
