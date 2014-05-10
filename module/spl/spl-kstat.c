99abc67e9d42956575f5fe0d6ee5a865720415ff
Jorgen Lundman <lundman@lundman.net>
2014-05-11 08:56:13 +0900

    Remove warnings regarding loff_t

--- module/spl/spl-kstat.c
+++ module/spl/spl-kstat.c
@@ -140,6 +140,6 @@ void
 __kstat_set_raw_ops(kstat_t *ksp,
                     int (*headers)(char *buf, size_t size),
                     int (*data)(char *buf, size_t size, void *data),
-                    void *(*addr)(kstat_t *ksp, loff_t index))
+                    void *(*addr)(kstat_t *ksp, off_t index))
 {
 }
