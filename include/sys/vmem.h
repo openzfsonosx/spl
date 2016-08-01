54401b2f6af2fbb536bdcae635077c8b8594d406
rottegift <rottegift@mac.com>
2016-08-01 13:06:26 +0100

    Change KMEM_QUANTUM to PAGESIZE

--- include/sys/vmem.h
+++ include/sys/vmem.h
@@ -34,9 +34,9 @@ extern "C" {
 
 
 // BGH - Back to 512k users reporting glitching, beachballing etc.
-#define KMEM_QUANTUM (PAGESIZE<<7) 
+//#define KMEM_QUANTUM (PAGESIZE<<7)
 
-//#define KMEM_QUANTUM (PAGESIZE) // (<<5, 128k, has been running for months (as of 23 sept 2015), lower does glitch
+#define KMEM_QUANTUM (PAGESIZE) // (<<5, 128k, has been running for months (as of 23 sept 2015), lower does glitch
 
 
 
