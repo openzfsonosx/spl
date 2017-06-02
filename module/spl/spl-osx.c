d1ee826cf21ebd8f2d3db82373265c761961a7d1
Jorgen Lundman <lundman@lundman.net>
2017-06-02 09:33:53 +0900

    Properly use org.openzfsonosx as project name for kexts

--- module/spl/spl-osx.c
+++ module/spl/spl-osx.c
@@ -486,7 +486,7 @@ extern kern_return_t _stop(kmod_info_t *ki, void *data);
 __private_extern__ kern_return_t spl_start(kmod_info_t *ki, void *data);
 __private_extern__ kern_return_t spl_stop(kmod_info_t *ki, void *data);
 
-__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(net.lundman.spl, "1.0.0", _start, _stop)
+__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(org.openzfsonosx.spl, "1.0.0", _start, _stop)
 __private_extern__ kmod_start_func_t *_realmain = spl_start;
 __private_extern__ kmod_stop_func_t *_antimain = spl_stop;
 __private_extern__ int _kext_apple_cc = __APPLE_CC__ ;
