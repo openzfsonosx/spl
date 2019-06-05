95cbaecb77e3f938d8f5e2e47fe581a517bd8a9d
Jorgen Lundman <lundman@lundman.net>
2019-06-05 10:11:24 +0900

    ZOL cpuid_features header file
    simd_x86.h is the header file used to detect CPU features, like AVX, SSE etc.
    
    We have moved it from linux/simd_x86.h to just sys/simd_x86.h
    
    To get at the CPU features, we need to add "cpuid_info()" to zfs.exports.
    
    Handle kernel/userland ZFS_ASM_BUG macro

--- module/spl/spl-processor.c
+++ module/spl/spl-processor.c
@@ -26,6 +26,7 @@
  */
 
 #include <sys/processor.h>
+#include <i386/cpuid.h>
 
 extern int cpu_number(void);
 
@@ -33,4 +34,20 @@ uint32_t
 getcpuid()
 {
 	return ((uint32_t)cpu_number());
+}
+
+uint64_t spl_cpuid_features(void)
+{
+	i386_cpu_info_t *info;
+
+	info = cpuid_info();
+	return info->cpuid_features;
+}
+
+uint64_t spl_cpuid_leaf7_features(void)
+{
+	i386_cpu_info_t *info;
+
+	info = cpuid_info();
+	return info->cpuid_leaf7_features;
 }
